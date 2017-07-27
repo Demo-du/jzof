bc1c965677f475225c8f4c533afb63c65152c659
Demo-du <1556141770@qq.com>
2017-07-27 19:07:22 +0800

    丑数

--- src/jzof/N49.java
+++ src/jzof/N49.java
@@ -7,7 +7,35 @@ public class N49 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        N49 n49=new N49();
+        System.out.println(n49.getUgly(15));
 	}
-
+    int getUgly(int index){
+    	if(index<0){
+    		return 0;
+    	}
+    	int []uglynum=new int[index];
+    	uglynum[0]=1;
+    	int m2=0;
+    	int m3=0;
+    	int m5=0;
+    	for(int i=1;i<index;i++){
+    		int v_min=min(uglynum[m2]*2,uglynum[m3]*3,uglynum[m5]*5);
+    		uglynum[i]=v_min;
+    		while(uglynum[m2]*2<=uglynum[i]){
+    			m2++;
+    		}
+    		while(uglynum[m3]*2<=uglynum[i]){
+    			m3++;
+    		}
+    		while(uglynum[m5]*2<=uglynum[i]){
+    			m5++;
+    		}
+    	}
+    	return uglynum[index-1];
+    }
+    public int min(int number1,int number2,int number3){
+		int min = (number1<number2)?number1:number2;
+		return min <number3?min:number3;
+	}
 }
