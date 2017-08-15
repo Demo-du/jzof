3cc4d78a972303d0cc65e34e93f8e0b77de79773
Demo-du <1556141770@qq.com>
2017-08-15 21:29:02 +0800

    singgle

---
+++ src/jzof/N3.java
@@ -0,0 +1,30 @@
+package jzof;
+
+import java.util.HashMap;
+
+public class N3 {
+      public static void print(int []shu){
+    	  HashMap<Integer,Integer> m=new HashMap<Integer,Integer>();
+    	  HashMap<Integer,Integer> f=new HashMap<Integer,Integer>();
+    	  boolean flag=false;
+    	    for(int a:shu){
+    	    	    if(m.containsKey(a)){
+    	    	    	    if(f.containsKey(a)){
+    	    	    	    	    
+    	    	    	    }else{
+    	    	    	            m.put(a, 1);
+    	    	    	            System.out.println(a);
+    	    	    	            f.put(a, 1);
+    	    	    	           // flag=true;
+    	    	    	    }
+    	    	    }else{
+    	    	    	    m.put(a, 0);
+    	    	    }
+    	    }
+    	    
+      }
+      public static void main(String []a){
+    	  int []ss={2,3,1,0,2,5,3,3};
+             print(ss);    	  
+      }
+}
