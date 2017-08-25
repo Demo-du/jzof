2d44ddda54065edf3b8896ee38adc70c80257e07
Demo-du <1556141770@qq.com>
2017-08-25 14:13:36 +0800

    数组中找重复

--- src/jzof/N3.java
+++ src/jzof/N3.java
@@ -3,28 +3,63 @@ package jzof;
 import java.util.HashMap;
 
 public class N3 {
-      /*public static void print(int []shu){
-    	  HashMap<Integer,Integer> m=new HashMap<Integer,Integer>();
-    	  HashMap<Integer,Integer> f=new HashMap<Integer,Integer>();
-    	  boolean flag=false;
-    	    for(int a:shu){
-    	    	    if(m.containsKey(a)){
-    	    	    	    if(f.containsKey(a)){
-    	    	    	    	    
-    	    	    	    }else{
-    	    	    	            m.put(a, 1);
-    	    	    	            System.out.println(a);
-    	    	    	            f.put(a, 1);
-    	    	    	           // flag=true;
-    	    	    	    }
-    	    	    }else{
-    	    	    	    m.put(a, 0);
-    	    	    }
-    	    }
-    	    
-      }*/
+	//问题1
+     public static boolean search1(int nums[]) {
+    	  int temp=0;
+    	  if(nums.length==0||nums==null) {
+    		  return false;
+    	  }
+    	  for(int i=0;i<nums.length;i++) {
+    		  while(nums[i]!=i) {
+    			  if(nums[i]==nums[nums[i]]) {
+    				  return true;
+    			  }
+    			  temp=nums[i];
+    			  nums[i]=nums[nums[i]];
+    			  nums[temp]=temp;
+    		  }
+    	  }
+    	  return false;
+      }
+     //问题2
+     static int  getchongfu(int nums[]) {
+    	 if(nums.length==0||nums==null) {
+    		 return 0;
+    	 }
+    	 int start=1,end=nums.length-1;
+    	 while(end>=start) {
+    		 int mid=(end+start)/2;
+    		 int count=countrange(nums,start,mid);
+    		 if(end==start) {
+    			 if(count>1) {
+    				 return start;
+    			 }else {
+    				 break;
+    			 }
+    		 }
+    		 if(count>(mid-start+1)) {//说明有重复数字
+    			 end=mid;
+    		 }else {
+    			 start=mid+1;
+    		 }
+    	 }
+    	 return -1;
+     }
+    static int countrange(int nums[],int start,int end) {//处在范围内的数字的数量
+    	 if(nums.length==0||nums==null) {
+    		 return 0;
+    	 }
+    	 int count=0;
+    	 for(int i=0;i<nums.length;i++) {
+    		 if(nums[i]>=start&&nums[i]<=end) {
+    			 count++;
+    		 }
+    	 }
+    	 return count;
+     }
       public static void main(String []a){
-    	  int []ss={2,3,1,0,2,5,3,3};
+    	  int []ss={2,3,1,4,2};
+    	  System.out.print(getchongfu(ss));
             // print(ss);    	  
       }
 }
