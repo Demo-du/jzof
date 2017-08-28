24210833bcc0d5e31b1d855b4e4f706837d1776f
Demo-du <1556141770@qq.com>
2017-08-28 10:30:00 +0800

    字符串替换空格

--- src/jzof/N5.java
+++ src/jzof/N5.java
@@ -1,13 +1,45 @@
 package jzof;
 
+import java.util.Arrays;
+
 public class N5 {
-    String rep(String s) {
-    	String res="";
-    	return "";
+    public static String rep(String s) {
+    	if(s==null||s.length()<=0) {
+    		return "";
+    	}
+    	int original_lenth=s.length();//字符串长度
+    	int count_blank=0;//空格数量
+    	
+    	for(int i=0;i<s.length();i++) {
+    		if(s.charAt(i)==' ') {
+    			count_blank++;
+    		}
+    	}
+    	//新字符串长度
+    	int new_lenth=s.length()+count_blank*2;
+    	//新字符串数组
+    	char [] shuzu=new char [new_lenth];
+    	for(int i=0;i<s.length();i++) {
+    		shuzu[i]=s.charAt(i);
+    	}
+    	int index_original=original_lenth-1;//指向使用过的字符串的尾部
+    	int index_new=new_lenth-1;//指向新字符串尾部
+    	while(index_original>=0&&index_new>index_original) {
+    		if(shuzu[index_original]==' ') {
+    			shuzu[index_new--]='0';
+    			shuzu[index_new--]='2';
+    			shuzu[index_new--]='%';
+    		}else {
+    			shuzu[index_new--]=shuzu[index_original];
+    			//index_new--;
+    		}
+    		index_original--;
+    	}
+    	return String.valueOf(shuzu);
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        System.out.println(rep("ww hasdf "));
 	}
 
 }
