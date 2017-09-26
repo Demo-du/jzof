354b03c93314d681b12822ce13cfcdb670ef8b86
Demo-du <1556141770@qq.com>
2017-09-26 20:31:58 +0800

    数的整数方

--- src/jzof/N16.java
+++ src/jzof/N16.java
@@ -7,17 +7,17 @@ public class N16 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(power(2,3));
+		System.out.println(power(2,6));
 
 	}
     static double power(double base,int exp){
-    	if(base==0){
+    	if(exp==0){
     		return 1;
     	}
-    	if(base==1){
+    	if(exp==1){
     		return base;
     	}
-    	double res=power(base,exp>>2);
+    	double res=power(base,exp>>1);
     	res*=res;
     	if((exp&0x1)==1){
     		res*=base;
