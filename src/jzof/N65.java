c55b6293de1bccca236f525226131c366d9ca40c
Demo-du <1556141770@qq.com>
2017-11-14 15:52:25 +0800

    不用加减乘除做加法

---
+++ src/jzof/N65.java
@@ -0,0 +1,25 @@
+package jzof;
+
+public class N65 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        N65 n65=new N65();
+        System.out.println(n65.Add(-2, 2));
+	}
+	public int Add(int num1,int num2){
+		int sum=0;
+		int carry=0;
+		while(num2!=0){
+			sum=num1^num2;
+			carry=(num1&num2)<<1;//进位
+			num1=sum;
+			num2=carry;
+		}
+		return num1;
+	}
+
+}
