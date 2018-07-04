0330579643839ecd4c35692fd7a5f5527590f87a
Demo-du <jianrobot@126.com>
2018-07-04 23:25:53 +0800

    oo

---
+++ src/jzof4/N12.java
@@ -0,0 +1,16 @@
+package jzof4;
+
+public class N12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public static boolean solve(int [][]map,int x,int y,String s,boolean[][]visited) {
+    	boolean res=false;
+    	int hang=map.length;
+    	int lie=map[0].length;
+    	
+    	return res;
+    }
+}
