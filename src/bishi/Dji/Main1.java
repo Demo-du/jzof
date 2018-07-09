a17573c7e32c3b26dc1dbdb6784bcb599caded72
Demo-du <jianrobot@126.com>
2018-07-09 21:46:06 +0800

    矩阵中的路径

---
+++ src/bishi/Dji/Main1.java
@@ -0,0 +1,12 @@
+package bishi.Dji;
+
+import java.util.Scanner;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc=new Scanner(System.in);
+	}
+
+}
--- src/jzof4/N12.java
+++ src/jzof4/N12.java
@@ -4,13 +4,38 @@ public class N12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        char [][]map= {
+        		{'a','b','t','g'},
+        		{'c','f','c','s'},
+        		{'j','d','e','h'}
+        };
+        boolean [][]visited=new boolean[3][4];
+        solve(map,0,0,"abfd",visited,0);
 	}
-    public static boolean solve(int [][]map,int x,int y,String s,boolean[][]visited) {
+    public static boolean solve(char [][]map,int x,int y,String s,boolean[][]visited,int len) {
     	boolean res=false;
     	int hang=map.length;
     	int lie=map[0].length;
-    	
+    	if(x<0||x>hang-1||y<0||y>lie-1) {
+    		return false;
+    	}
+    	if(len>=4) {
+    		System.out.println("sss");
+    		return true;
+    	}
+    	if(map[x][y]==s.charAt(len)) {
+    		len++;
+    		visited[x][y]=true;
+    	}else {
+    		return false;
+    	}
+    	res=solve(map,x+1,y,s,visited,len)||
+    			solve(map,x,y+1,s,visited,len)||
+    			solve(map,x-1,y,s,visited,len)||
+    			solve(map,x,y-1,s,visited,len);
+    	if(!res) {
+    		visited[x][y]=false;
+    	}
     	return res;
     }
 }
