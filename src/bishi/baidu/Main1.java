22389a72125117331d8b7ad6ac7306516d78f514
Demo-du <1556141770@qq.com>
2018-09-12 20:00:27 +0800

    'xunlei'

--- src/bishi/baidu/Main1.java
+++ src/bishi/baidu/Main1.java
@@ -9,7 +9,12 @@ public class Main1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         while (sc.hasNext()){
+            String name=sc.nextLine().trim();
+            char c=sc.nextLine().trim().charAt(0);
+            String res="";
+            for (int i=0;i<name.length();i++){
 
+            }
         }
     }
 }
