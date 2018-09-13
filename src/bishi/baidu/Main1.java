7fc50bb4806aee8b9d99ddb57b245ce00adbc8e7
Demo-du <1556141770@qq.com>
2018-09-13 11:32:21 +0800

    'string'

--- src/bishi/baidu/Main1.java
+++ src/bishi/baidu/Main1.java
@@ -1,19 +1,36 @@
 package bishi.baidu;
 
+
 import java.util.Scanner;
 
-/**
- * Created by Administrator on 2018\9\11 0011.
- */
 public class Main1 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        while (sc.hasNext()){
-            String name=sc.nextLine().trim();
-            char c=sc.nextLine().trim().charAt(0);
-            String res="";
-            for (int i=0;i<name.length();i++){
+        Scanner sc =new Scanner(System.in);
+        while(sc.hasNext()){
+            String str = sc.nextLine();
+            String c =sc.next();
 
+            char[] res = new char[str.length()+1];
+            int i=0;
+            while(str.charAt(i)- c.charAt(0) <=0){
+                i++;
+            }
+            int j = 0;
+            int index = 0;
+            while (j<i){
+                res[index] = str.charAt(j);
+                j++;
+                index++;
+            }
+            res[index++] = c.charAt(0);
+
+            while (j<str.length()){
+                res[index] = str.charAt(j);
+                j++;
+                index++;
+            }
+            for (int k = 0; k <res.length ; k++) {
+                System.out.print(res[k]);
             }
         }
     }
